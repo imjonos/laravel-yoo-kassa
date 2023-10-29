80d45f14e520ece84d922726c593c659fcde8d55
Eugeny Nosenko <info@coders.studio>
2023-10-29 22:45:31 +0300

    use HasUuids

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Nos\Yookassa\Http\Middleware\IpAccess;
 
-Route::group(['middleware' => ['web', IpAccess::class]], function () {
+Route::group(['middleware' => [IpAccess::class]], function () {
     Route::namespace('Nos\Yookassa\Http\Controllers')->group(function () {
         Route::post('/yookassa/notifications', 'NotificationController@index')->name('yookassa.notifications');
     });
